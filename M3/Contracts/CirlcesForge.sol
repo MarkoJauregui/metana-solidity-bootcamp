20618d990c46457fc8546901a2f5b09b7cbcd163
Maruko-coding <markojau@gmail.com>
2023-10-05 13:43:18 -0300

    Forge Token 3

--- M3/Contracts/CirlcesForge.sol
+++ M3/Contracts/CirlcesForge.sol
@@ -4,6 +4,11 @@ pragma solidity ^0.8.7;
 import "./CirclesERC1155.sol";
 
 contract CirclesForge {
+    // Custom Errors
+    error CirclesForge__InsufficientToken0();
+    error CirclesForge__InsufficientToken1();
+    error CirclesForge__InsufficientToken2();
+
     // Storage: Instance of the ERC1155 contract
     CirclesERC1155 private s_erc1155Contract;
 
@@ -15,5 +20,24 @@ contract CirclesForge {
         s_erc1155Contract = CirclesERC1155(_erc1155Address);
     }
 
-    // ... Forging functions to come ...
+    /**
+     * @dev Forges Token 3 by burning tokens 0 & 1
+     */
+    function forgeToken3() external {
+        // Check caller has enough token 0 & 1 to burn
+        if (s_erc1155Contract.balanceOf(msg.sender, CirclesERC1155.TOKEN_ID_0) < 1) {
+            revert CirclesForge__InsufficientToken0();
+        }
+        if (s_erc1155Contract.balanceOf(msg.sender, CirclesERC1155.TOKEN_ID_1) < 1) {
+            revert CirclesForge__InsufficientToken0();
+        }
+
+        // Burn tokens 0 & 1 from caller account
+        s_erc1155Contract.burn(msg.sender, CirclesERC1155.TOKEN_ID_0, 1);
+        s_erc1155Contract.burn(msg.sender, CirclesERC1155.TOKEN_ID_1, 1);
+
+
+        // Mint Token 3 to caller account
+        s_erc1155Contract.mint(msg.sender, CirclesERC1155)
+    }
 }

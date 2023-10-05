e1caee01c4a075dcef820dad8f072b41af837816
Maruko-coding <markojau@gmail.com>
2023-10-05 13:54:42 -0300

    forgeToken 4, 5 & 6

--- M3/Contracts/CirlcesForge.sol
+++ M3/Contracts/CirlcesForge.sol
@@ -36,8 +36,65 @@ contract CirclesForge {
         s_erc1155Contract.burn(msg.sender, CirclesERC1155.TOKEN_ID_0, 1);
         s_erc1155Contract.burn(msg.sender, CirclesERC1155.TOKEN_ID_1, 1);
 
-
         // Mint Token 3 to caller account
-        s_erc1155Contract.mint(msg.sender, CirclesERC1155)
+        s_erc1155Contract.mint(msg.sender, CirclesERC1155.TOKEN_ID_3, 1);
+    }
+
+    /**
+     * @dev Forges Token 4 by burning Tokens 1 & 2
+     */
+    function forgeToken4() external {
+        if (s_erc1155Contract.balanceOf(msg.sender, CirclesERC1155.TOKEN_ID_1) < 1) {
+            revert CirclesForge__InsufficientToken1();
+        }
+        if (s_erc1155Contract.balanceOf(msg.sender, CirclesERC1155.TOKEN_ID_2) < 1) {
+            revert CirclesForge__InsufficientToken2();
+        }
+
+        s_erc1155Contract.burn(msg.sender, CirclesERC1155.TOKEN_ID_1, 1);
+        s_erc1155Contract.burn(msg.sender, CirclesERC1155.TOKEN_ID_2, 1);
+
+        s_erc1155Contract.mint(msg.sender, CirclesERC1155.TOKEN_ID_4, 1);
+    }
+
+    /**
+     * @dev Forges Token 5 by burning tokens 0 and 2.
+     */
+    function forgeToken5() external {
+        if (s_erc1155Contract.balanceOf(msg.sender, CirclesERC1155.TOKEN_ID_0) < 1) {
+            revert CirclesForge__InsufficientToken0();
+        }
+        if (s_erc1155Contract.balanceOf(msg.sender, CirclesERC1155.TOKEN_ID_2) < 1) {
+            revert CirclesForge__InsufficientToken2();
+        }
+
+        s_erc1155Contract.burn(msg.sender, CirclesERC1155.TOKEN_ID_0, 1);
+        s_erc1155Contract.burn(msg.sender, CirclesERC1155.TOKEN_ID_2, 1);
+
+        s_erc1155Contract.mint(msg.sender, CirclesERC1155.TOKEN_ID_5, 1);
+    }
+
+    /**
+     * @dev Forges Token 6 by burning tokens 0, 1, and 2.
+     */
+    function forgeToken6() external {
+        // Ensure the caller has enough of tokens 0, 1, and 2 to burn
+        if (s_erc1155Contract.balanceOf(msg.sender, CirclesERC1155.TOKEN_ID_0) < 1) {
+            revert CirclesForge__InsufficientToken0();
+        }
+        if (s_erc1155Contract.balanceOf(msg.sender, CirclesERC1155.TOKEN_ID_1) < 1) {
+            revert CirclesForge__InsufficientToken1();
+        }
+        if (s_erc1155Contract.balanceOf(msg.sender, CirclesERC1155.TOKEN_ID_2) < 1) {
+            revert CirclesForge__InsufficientToken2();
+        }
+
+        // Burn tokens 0, 1, and 2 from caller's account
+        s_erc1155Contract.burn(msg.sender, CirclesERC1155.TOKEN_ID_0, 1);
+        s_erc1155Contract.burn(msg.sender, CirclesERC1155.TOKEN_ID_1, 1);
+        s_erc1155Contract.burn(msg.sender, CirclesERC1155.TOKEN_ID_2, 1);
+
+        // Mint Token 6 to caller's account
+        s_erc1155Contract.mint(msg.sender, CirclesERC1155.TOKEN_ID_6, 1);
     }
 }

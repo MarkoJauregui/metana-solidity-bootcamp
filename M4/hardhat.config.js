1f8a3e78cf61c13f48f38736b60af389b86b07a5
Maruko-coding <markojau@gmail.com>
2023-10-11 18:26:28 -0300

    package fixes

--- M4/hardhat.config.js
+++ M4/hardhat.config.js
@@ -1,21 +1,22 @@
 require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-ethers');
+require('solidity-coverage');
+
+// Include any other plugins you might need
 
 module.exports = {
 	defaultNetwork: 'hardhat',
 	networks: {
 		hardhat: {
-			accounts: {
-				mnemonic: 'test test test test test test test test test test test junk',
-			},
-			gasPrice: 20000000000,
-			initialBaseFeePerGas: 0,
+			// Configuration for the local hardhat network
+			chainId: 1337,
 		},
+		// Add configurations for other networks like mainnet, ropsten, etc., if needed
 	},
 	solidity: {
 		compilers: [
 			{
-				version: '0.8.0',
+				version: '0.8.20', // Adjust this to match your contract's pragma statement
 				settings: {
 					optimizer: {
 						enabled: true,
@@ -23,15 +24,17 @@ module.exports = {
 					},
 				},
 			},
-			{
-				version: '0.8.20',
-				settings: {
-					optimizer: {
-						enabled: true,
-						runs: 200,
-					},
-				},
-			},
+			// Add other compiler versions if your project requires multiple versions
 		],
 	},
+	paths: {
+		artifacts: './artifacts',
+		cache: './cache',
+		sources: './contracts',
+		tests: './test',
+	},
+	mocha: {
+		timeout: 40000,
+	},
+	// Add other configurations if necessary
 };

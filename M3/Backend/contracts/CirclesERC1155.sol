20991b808c3f44e2813aceeb83eddee6053bd7fa
Maruko-coding <markojau@gmail.com>
2023-10-16 14:55:42 -0300

    CirclesERC1155 issues fix

--- M3/Backend/contracts/CirclesERC1155.sol
+++ M3/Backend/contracts/CirclesERC1155.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/AccessControlDefaultAdminRules.sol";
 
 error CirclesERC1155__CooldownNotElapsed();
 error CirclesERC1155__InvalidTokenForTrade();
@@ -14,11 +14,15 @@ error CirclesERC1155__NotAMinter();
  * @title CirclesERC1155
  * @dev A custom ERC1155 token with specific minting, burning, and trading logic.
  */
-contract CirclesERC1155 is ERC1155, AccessControl {
+contract CirclesERC1155 is ERC1155, AccessControlDefaultAdminRules {
     // Constants for token IDs
     uint256 public constant TOKEN_ID_0 = 0;
     uint256 public constant TOKEN_ID_1 = 1;
     uint256 public constant TOKEN_ID_2 = 2;
+    uint256 public constant TOKEN_ID_3 = 3;
+    uint256 public constant TOKEN_ID_4 = 4;
+    uint256 public constant TOKEN_ID_5 = 5;
+    uint256 public constant TOKEN_ID_6 = 6;
 
     /// @notice Role identifier for minters
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
@@ -28,7 +32,7 @@ contract CirclesERC1155 is ERC1155, AccessControl {
 
     // Storage variables
     address private s_forgingContract;
-    mapping(address => mapping(uint256 => uint256)) private s_lastMintTimestamp;
+    mapping(address => uint256) private s_lastMintTimestamp; // Adjusted for global cooldown
 
     // Events
     event Minted(address indexed user, uint256 indexed tokenId, uint256 amount);
@@ -45,31 +49,30 @@ contract CirclesERC1155 is ERC1155, AccessControl {
      */
     constructor()
         ERC1155("ipfs://QmdpEYwJircF4qH5imJG4bJT3TH6rNYxzCL2d8B4bG7Uhy/{id}")
-    {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    }
+        AccessControlDefaultAdminRules(1 days, msg.sender)
+    {}
 
     /**
      * @notice Allows minting of tokens.
-
      * @param tokenId The ID of the token to mint.
-     * @param amount The number of tokens to mint.
      */
-    function mint(address account, uint256 tokenId, uint256 amount) public {
+    function mint(address account, uint256 tokenId) public {
         if (tokenId <= TOKEN_ID_2) {
             if (
-                block.timestamp - s_lastMintTimestamp[msg.sender][tokenId] <
+                block.timestamp - s_lastMintTimestamp[msg.sender] <
                 COOLDOWN_TIME
-            ) revert CirclesERC1155__CooldownNotElapsed();
-
-            s_lastMintTimestamp[msg.sender][tokenId] = block.timestamp;
+            ) {
+                revert CirclesERC1155__CooldownNotElapsed();
+            }
+            s_lastMintTimestamp[msg.sender] = block.timestamp;
         } else {
-            if (!hasRole(MINTER_ROLE, msg.sender))
+            if (!hasRole(MINTER_ROLE, msg.sender)) {
                 revert CirclesERC1155__NotAMinter();
+            }
         }
 
-        _mint(account, tokenId, amount, "");
-        emit Minted(msg.sender, tokenId, amount);
+        _mint(account, tokenId, 1, ""); // Amount set to 1
+        emit Minted(msg.sender, tokenId, 1); // Amount set to 1
     }
 
     /**
@@ -77,29 +80,30 @@ contract CirclesERC1155 is ERC1155, AccessControl {
      * @param tokenId The ID of the token to trade.
      * @param desiredToken The ID of the desired token.
      */
-    function tradeToken(
-        uint256 tokenId,
-        uint256 desiredToken,
-        uint256 amount
-    ) public {
-        if (!(tokenId >= 3 && tokenId <= 6))
+    function tradeToken(uint256 tokenId, uint256 desiredToken) public {
+        if (!(tokenId >= 3 && tokenId <= 6)) {
             revert CirclesERC1155__InvalidTokenForTrade();
-        if (!(desiredToken >= TOKEN_ID_0 && desiredToken <= TOKEN_ID_2))
+        }
+        if (!(desiredToken >= TOKEN_ID_0 && desiredToken <= TOKEN_ID_2)) {
             revert CirclesERC1155__InvalidTokenForTrade();
+        }
 
-        _burn(msg.sender, tokenId, amount);
-        mint(msg.sender, desiredToken, amount);
-        emit Traded(msg.sender, tokenId, desiredToken, amount);
+        _burn(msg.sender, tokenId, 1); // Amount set to 1
+        mint(msg.sender, desiredToken); // Amount set to 1 implicitly
+        emit Traded(msg.sender, tokenId, desiredToken, 1); // Amount set to 1
     }
 
     /**
      * @notice Allows burning of tokens.
      * @param tokenId The ID of the token to burn.
-     * @param amount The number of tokens to burn.
      */
-    function burn(address account, uint256 tokenId, uint256 amount) public {
-        _burn(account, tokenId, amount);
-        emit Burned(account, tokenId, amount);
+    function burn(address account, uint256 tokenId) public {
+        if (tokenId < TOKEN_ID_3 || tokenId > TOKEN_ID_6) {
+            revert CirclesERC1155__InvalidTokenForBurn();
+        }
+
+        _burn(account, tokenId, 1); // Amount set to 1
+        emit Burned(account, tokenId, 1); // Amount set to 1
     }
 
     /**
@@ -123,19 +127,22 @@ contract CirclesERC1155 is ERC1155, AccessControl {
      */
     function supportsInterface(
         bytes4 interfaceId
-    ) public view override(ERC1155, AccessControl) returns (bool) {
+    )
+        public
+        view
+        override(ERC1155, AccessControlDefaultAdminRules)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
 
     /**
-     * @notice Retrieves the last mint timestamp for a user's specific token.
+     * @notice Retrieves the last mint timestamp for a user.
      * @param user The address of the user.
-     * @param tokenId The ID of the token.
      */
     function getLastMintTimestamp(
-        address user,
-        uint256 tokenId
+        address user
     ) external view returns (uint256) {
-        return s_lastMintTimestamp[user][tokenId];
+        return s_lastMintTimestamp[user];
     }
 }

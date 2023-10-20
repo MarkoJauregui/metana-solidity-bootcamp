eb1274390c47509ff70e2b6482c0267752035f3f
Maruko-coding <markojau@gmail.com>
2023-10-20 14:11:11 -0300

    USDC update

--- M5/alchemy-charts/src/components/ERC20Volume/ERC20Volume.js
+++ M5/alchemy-charts/src/components/ERC20Volume/ERC20Volume.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 import alchemy from '../../utils/alchemy';
 
-const DAI_CONTRACT_ADDRESS = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+const USDC_CONTRACT_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
 const ERC20Volume = () => {
@@ -18,7 +18,7 @@ const ERC20Volume = () => {
 			const response = await alchemy.core.getAssetTransfers({
 				fromBlock: '0x0',
 				toBlock: 'latest',
-				contractAddresses: [DAI_CONTRACT_ADDRESS],
+				contractAddresses: [USDC_CONTRACT_ADDRESS],
 				excludeZeroValue: true,
 				category: ['erc20'],
 				maxCount: '0x3e8',
@@ -52,7 +52,7 @@ const ERC20Volume = () => {
 				const fetchedTransfers = await getLast10BlocksTransfers();
 				setTransfers(fetchedTransfers);
 			} catch (error) {
-				console.error('Error fetching DAI transfers:', error);
+				console.error('Error fetching USDCtransfers:', error);
 			} finally {
 				setIsLoading(false);
 			}
@@ -105,7 +105,7 @@ const ERC20Volume = () => {
 
 	const series = [
 		{
-			name: 'DAI Transfers',
+			name: 'USDC Transfers',
 			data: transfers.map((t) => t.value),
 		},
 	];

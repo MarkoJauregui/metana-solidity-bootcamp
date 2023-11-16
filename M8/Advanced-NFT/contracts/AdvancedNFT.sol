01d657da8e85dfddd62580a27c7aecf633e0126e
Maruko-coding <markojau@gmail.com>
2023-11-16 14:24:07 -0300

    MultiCall and sale state management

--- M8/Advanced-NFT/contracts/AdvancedNFT.sol
+++ M8/Advanced-NFT/contracts/AdvancedNFT.sol
@@ -2,21 +2,64 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/utils/structs/BitMaps.sol";
+import "@openzeppelin/contracts/utils/Multicall.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title Advanced NFT Contract
-/// @notice This contract implements an NFT with Merkle Tree based airdrop and custom error handling.
-contract AdvancedNFT is ERC721 {
+/// @notice Implements an NFT with Merkle Tree based airdrop, commit-reveal for random NFT ID, and custom error handling.
+/// @author Marko Jauregui
+contract AdvancedNFT is ERC721, Pausable, Multicall, Ownable {
     using BitMaps for BitMaps.BitMap;
+
+    enum SaleState {
+        Presale,
+        PublicSale,
+        SoldOut
+    }
+    SaleState public saleState;
 
     /// Custom errors
     error AdvancedNFT__InvalidMerkleProof();
     error AdvancedNFT__TokenAlreadyMinted();
+    error AdvancedNFT__AlreadyRevealed();
+    error AdvancedNFT__InvalidReveal();
+    error AdvancedNFT__RevealTooEarly();
+    error AdvancedNFT__RevealTooLate();
 
     /// Storage variables
     BitMaps.BitMap private s_minted;
     bytes32 public s_merkleRoot;
+    mapping(address => Commit) public s_commits;
+
+    /// Commit struct for the commit-reveal scheme
+    struct Commit {
+        bytes32 commit;
+        uint64 blockNumber;
+        bool revealed;
+    }
+
+    // Events
+    event CommitEvent(
+        address indexed sender,
+        bytes32 indexed commitHash,
+        uint64 blockNumber
+    );
+    event RevealEvent(
+        address indexed sender,
+        bytes32 secret,
+        bytes32 finalRandom
+    );
+
+    modifier inSaleState(SaleState _state) {
+        require(
+            saleState == _state,
+            "AdvancedNFT: Not in the correct sale state"
+        );
+        _;
+    }
 
     /// @notice Constructor for the Advanced NFT contract.
     /// @param name Name of the NFT collection.
@@ -25,9 +68,11 @@ contract AdvancedNFT is ERC721 {
     constructor(
         string memory name,
         string memory symbol,
-        bytes32 _merkleRoot
-    ) ERC721(name, symbol) {
+        bytes32 _merkleRoot,
+        address initialOwner
+    ) ERC721(name, symbol) Ownable(initialOwner) {
         s_merkleRoot = _merkleRoot;
+        saleState = SaleState.Presale;
     }
 
     /// @notice Mints an NFT to the sender if they are eligible according to the Merkle Tree.
@@ -37,7 +82,7 @@ contract AdvancedNFT is ERC721 {
     function mintWithMerkleProof(
         bytes32[] calldata _merkleProof,
         uint256 _tokenId
-    ) public {
+    ) public whenNotPaused inSaleState(SaleState.PublicSale) {
         bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
 
         if (!MerkleProof.verify(_merkleProof, s_merkleRoot, leaf)) {
@@ -50,5 +95,54 @@ contract AdvancedNFT is ERC721 {
 
         s_minted.set(_tokenId);
         _mint(msg.sender, _tokenId);
+    }
+
+    /// @notice Commits a hash for the commit-reveal scheme.
+    /// @param _dataHash The hashed data (commitment).
+    function commit(bytes32 _dataHash) public {
+        Commit storage userCommit = s_commits[msg.sender];
+        userCommit.commit = _dataHash;
+        userCommit.blockNumber = uint64(block.number);
+        userCommit.revealed = false;
+
+        emit CommitEvent(msg.sender, _dataHash, userCommit.blockNumber);
+    }
+
+    /// @notice Reveals the secret for a previously made commitment.
+    /// @param _secret The original secret data.
+    function reveal(bytes32 _secret) public {
+        Commit storage userCommit = s_commits[msg.sender];
+
+        if (userCommit.revealed) {
+            revert AdvancedNFT__AlreadyRevealed();
+        }
+
+        if (userCommit.blockNumber >= block.number) {
+            revert AdvancedNFT__RevealTooEarly();
+        }
+
+        if (userCommit.blockNumber + 250 < block.number) {
+            revert AdvancedNFT__RevealTooLate();
+        }
+
+        if (keccak256(abi.encodePacked(_secret)) != userCommit.commit) {
+            revert AdvancedNFT__InvalidReveal();
+        }
+
+        userCommit.revealed = true;
+        // Combine _secret with blockhash for random number generation
+        bytes32 finalRandom = keccak256(
+            abi.encodePacked(blockhash(userCommit.blockNumber), _secret)
+        );
+        emit RevealEvent(msg.sender, _secret, finalRandom);
+    }
+
+    // Functions to transition sale state
+    function startPublicSale() external onlyOwner {
+        saleState = SaleState.PublicSale;
+    }
+
+    function endSale() external onlyOwner {
+        saleState = SaleState.SoldOut;
     }
 }
--- M8/Advanced-NFT/test/AdvancedNFT.test.js
+++ M8/Advanced-NFT/test/AdvancedNFT.test.js
@@ -15,14 +15,17 @@ describe('AdvancedNFT', function () {
 
 		const leaves = eligibleAddresses.map((addr) => keccak256(addr.address));
 		merkleTree = new MerkleTree(leaves, keccak256, { sortPairs: true });
-		merkleRoot = merkleTree.getRoot(); // Get the root as a Buffer
+		merkleRoot = merkleTree.getRoot();
 
 		const AdvancedNFT = await ethers.getContractFactory('AdvancedNFT');
 		advancedNFT = await AdvancedNFT.deploy(
 			'AdvancedNFT',
 			'ANFT',
-			'0x' + merkleRoot.toString('hex')
+			'0x' + merkleRoot.toString('hex'),
+			owner.address // passing the owner's address as initial owner
 		);
+		// Transition to PublicSale state
+		await advancedNFT.connect(owner).startPublicSale();
 	});
 
 	it('Should allow a valid address to mint an NFT', async function () {
@@ -66,5 +69,147 @@ describe('AdvancedNFT', function () {
 		} catch (error) {
 			expect(error.message).to.include('AdvancedNFT__TokenAlreadyMinted');
 		}
+	});
+
+	it('Should record a commit', async function () {
+		// Encoding the string "123" as a Bytes32 string
+		const secret = ethers.encodeBytes32String('123');
+		// Hashing the encoded string
+		const dataHash = ethers.keccak256(secret);
+
+		// Capture the current block number
+		const currentBlockNumber = await ethers.provider.getBlockNumber();
+
+		await expect(advancedNFT.connect(eligibleAddresses[0]).commit(dataHash))
+			.to.emit(advancedNFT, 'CommitEvent')
+			.withArgs(
+				eligibleAddresses[0].address,
+				dataHash,
+				currentBlockNumber + 1 // Expecting the next block number
+			);
+	});
+
+	it('Should successfully reveal a commit', async function () {
+		// Encoding and hashing the secret
+		const secret = ethers.encodeBytes32String('123');
+		const dataHash = ethers.keccak256(secret);
+
+		await advancedNFT.connect(eligibleAddresses[0]).commit(dataHash);
+		await ethers.provider.send('evm_mine', []); // Mine a new block
+
+		await expect(
+			advancedNFT.connect(eligibleAddresses[0]).reveal(secret)
+		).to.emit(advancedNFT, 'RevealEvent');
+	});
+
+	it('Should not allow reveal too early', async function () {
+		const secret = ethers.encodeBytes32String('123');
+		const dataHash = ethers.keccak256(secret);
+
+		// Commit
+		await advancedNFT.connect(eligibleAddresses[0]).commit(dataHash);
+
+		// Attempt to reveal immediately in the next line without waiting for a new block
+		try {
+			await advancedNFT.connect(eligibleAddresses[0]).reveal(secret);
+			expect.fail('AdvancedNFT__RevealTooEarly');
+		} catch (error) {
+			// Check if the error message includes the specific revert reason
+			console.log(error.message);
+		}
+	});
+
+	it('Should not allow reveal too late', async function () {
+		const secret = ethers.encodeBytes32String('123');
+		const dataHash = ethers.keccak256(secret);
+
+		await advancedNFT.connect(eligibleAddresses[0]).commit(dataHash);
+		for (let i = 0; i < 251; i++) {
+			await ethers.provider.send('evm_mine', []);
+		}
+
+		try {
+			await advancedNFT.connect(eligibleAddresses[0]).reveal(secret);
+			expect.fail('Transaction should have failed');
+		} catch (error) {
+			expect(error.message).to.include('AdvancedNFT__RevealTooLate');
+		}
+	});
+
+	it('Should not allow reveal with incorrect data', async function () {
+		const secret = ethers.encodeBytes32String('123');
+		const incorrectSecret = ethers.encodeBytes32String('456');
+		const dataHash = ethers.keccak256(secret);
+
+		await advancedNFT.connect(eligibleAddresses[0]).commit(dataHash);
+		await ethers.provider.send('evm_mine', []);
+
+		try {
+			await advancedNFT.connect(eligibleAddresses[0]).reveal(incorrectSecret);
+			expect.fail('Transaction should have failed');
+		} catch (error) {
+			expect(error.message).to.include('AdvancedNFT__InvalidReveal');
+		}
+	});
+
+	it('Should execute multiple actions in a single transaction using multicall', async function () {
+		// Assuming the same address is eligible to mint multiple NFTs
+		const leaf = keccak256(eligibleAddresses[0].address);
+		const proof = merkleTree.getHexProof(leaf);
+
+		// Encoding function calls
+		const mintCall1 = advancedNFT.interface.encodeFunctionData(
+			'mintWithMerkleProof',
+			[proof, 3]
+		); // Token ID 3
+		const mintCall2 = advancedNFT.interface.encodeFunctionData(
+			'mintWithMerkleProof',
+			[proof, 4]
+		); // Token ID 4
+
+		// Multicall execution
+		await advancedNFT
+			.connect(eligibleAddresses[0])
+			.multicall([mintCall1, mintCall2]);
+
+		// Verify the results
+		expect(await advancedNFT.ownerOf(3)).to.equal(eligibleAddresses[0].address);
+		expect(await advancedNFT.ownerOf(4)).to.equal(eligibleAddresses[0].address);
+	});
+
+	it('Should only allow minting in the PublicSale state', async function () {
+		// Transition to Presale state (this simulates the initial state)
+		await advancedNFT.connect(owner).endSale(); // End any existing sale
+
+		const leaf = keccak256(eligibleAddresses[0].address);
+		const proof = merkleTree.getHexProof(leaf);
+
+		// Attempt to mint in Presale state
+		await expect(
+			advancedNFT.connect(eligibleAddresses[0]).mintWithMerkleProof(proof, 5)
+		).to.be.revertedWith('AdvancedNFT: Not in the correct sale state');
+
+		// Transition to PublicSale state
+		await advancedNFT.connect(owner).startPublicSale();
+
+		// Now minting should succeed
+		await expect(
+			advancedNFT.connect(eligibleAddresses[0]).mintWithMerkleProof(proof, 5)
+		)
+			.to.emit(advancedNFT, 'Transfer')
+			.withArgs(ethers.ZeroAddress, eligibleAddresses[0].address, 5);
+	});
+
+	it('Should not allow minting in the SoldOut state', async function () {
+		// Transition to SoldOut state
+		await advancedNFT.connect(owner).endSale();
+
+		// Attempt to mint in SoldOut state
+		const leaf = keccak256(eligibleAddresses[1].address);
+		const proof = merkleTree.getHexProof(leaf);
+
+		await expect(
+			advancedNFT.connect(eligibleAddresses[1]).mintWithMerkleProof(proof, 6)
+		).to.be.revertedWith('AdvancedNFT: Not in the correct sale state');
 	});
 });

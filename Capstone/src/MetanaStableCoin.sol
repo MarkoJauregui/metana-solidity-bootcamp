acfad9c89c5474690e8c5c895c6ba8caa0e4cc91
Maruko-coding <markojau@gmail.com>
2024-02-26 15:30:50 -0300

    Project setup

---
+++ Capstone/src/MetanaStableCoin.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// Imports
+//---------------------------
+
+/**
+ * @title MetanaStableCoin
+ * @author Marko Jauregui
+ * @notice This contract is governed by the MSCEngine contract.This is just the ERC20 implementation.
+ */
+contract MetanaStableCoin {
+    constructor() {}
+}

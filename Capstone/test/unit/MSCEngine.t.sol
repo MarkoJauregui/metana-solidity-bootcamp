4d09bb8db49b5a587184268587e0228e36af80b2
Maruko-coding <markojau@gmail.com>
2024-02-28 12:19:41 -0300

    Initial unit tests

---
+++ Capstone/test/unit/MSCEngine.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployMSC} from "../../script/DeployMSC.s.sol";
+import {MetanaStableCoin} from "../../src/MetanaStableCoin.sol";
+import {MSCEngine} from "../../src/MSCEngine.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "../mocks/ERC20Mock.sol";
+
+contract MSCEngineTest is Test {
+    DeployMSC deployer;
+    MetanaStableCoin msc;
+    MSCEngine engine;
+    HelperConfig config;
+    address ethUsdPriceFeed;
+    address weth;
+
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+
+    function setUp() public {
+        deployer = new DeployMSC();
+        (msc, engine, config) = deployer.run();
+        (ethUsdPriceFeed,, weth,,) = config.activeNetworkConfig();
+
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
+    }
+
+    ///////////////////
+    //  Price Tests //
+    //////////////////
+
+    function testGetUsdValue() public {
+        uint256 ethAmount = 15e18;
+        uint256 expectedUsd = 30000e18;
+        uint256 actualUsd = engine.getUsdValue(weth, ethAmount);
+        assertEq(expectedUsd, actualUsd);
+    }
+
+    // Deposit Collateral //
+    function testRevertsIfCollateralIsZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(engine), AMOUNT_COLLATERAL);
+
+        vm.expectRevert(MSCEngine.MSCEngine__NeedsMoreThanZero.selector);
+        engine.depositCollateral(weth, 0);
+        vm.stopPrank();
+    }
+}
